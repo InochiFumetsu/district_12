aa086bafa22970a8056c9e5814d66e087d07673e
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 16:34:36 -0500

    update version No.

--- 1042_Lab1B_group12.py
+++ 1042_Lab1B_group12.py
@@ -1,5 +1,5 @@
 #THIS IS THE MAIN PROJECT FILE WHICH WILL IMPORT THE OTHER TWO MODULES
-#version: 0.0
+#version: 0.3.0
 
 import importlib as imp               # Spencer: I am not sure of the syntax they will teach us for this in class (don't know if this is how they'll want us to do it)
 imp.import_module(load_data_12)       #          I'm just including it to kind of show what I *THINK* they're going to have us do for the 2 "modules" we have to write.
