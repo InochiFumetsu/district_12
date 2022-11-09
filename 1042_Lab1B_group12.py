ed31c0ff77890cbf92aa18bfbdd23946ed694bcd
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 08:35:30 -0500

    Update 1042_Lab1B_group12.py

--- 1042_Lab1B_group12.py
+++ 1042_Lab1B_group12.py
@@ -4,3 +4,5 @@
 import importlib as imp               # Spencer: I am not sure of the syntax they will teach us for this in class (don't know if this is how they'll want us to do it)
 imp.import_module(load_data_12.py)    #          I'm just including it to kind of show what I *THINK* they're going to have us do for the 2 "modules" we have to write.
 imp.import_module(sort_plot_12.py)
+
+#STEP 1: Gonna go ahead and edit something here in the branch to be different from the master.
