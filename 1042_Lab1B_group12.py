7a1aa4a6830fe3e33ededf8cf927427f732c7cbd
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 16:28:40 -0500

    Update 1042_Lab1B_group12.py
    fixed minor syntactical error to import_module arguments.

--- 1042_Lab1B_group12.py
+++ 1042_Lab1B_group12.py
@@ -2,8 +2,8 @@
 #version: 0.0
 
 import importlib as imp               # Spencer: I am not sure of the syntax they will teach us for this in class (don't know if this is how they'll want us to do it)
-imp.import_module(load_data_12.py)    #          I'm just including it to kind of show what I *THINK* they're going to have us do for the 2 "modules" we have to write.
-imp.import_module(sort_plot_12.py)
+imp.import_module(load_data_12)    #          I'm just including it to kind of show what I *THINK* they're going to have us do for the 2 "modules" we have to write.
+imp.import_module(sort_plot_12)
 
 # Ok so it SEEMS as though you only have to remove the ">>>>>>>master" and "<<<<<<<slave" lines of "code" in the file it shows you highlighting the conflicts in
 # order to have the option to mark the file as "conflicts resolved" (top right button) -- actually, on further testing, you only need to remove the "<<<<<<<"
