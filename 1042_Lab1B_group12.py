3f7ec2090c7efd154bf1ef051b8b6f447bcf1a64
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 08:36:36 -0500

    Update 1042_Lab1B_group12.py

--- 1042_Lab1B_group12.py
+++ 1042_Lab1B_group12.py
@@ -4,3 +4,5 @@
 import importlib as imp               # Spencer: I am not sure of the syntax they will teach us for this in class (don't know if this is how they'll want us to do it)
 imp.import_module(load_data_12.py)    #          I'm just including it to kind of show what I *THINK* they're going to have us do for the 2 "modules" we have to write.
 imp.import_module(sort_plot_12.py)
+
+# STEP 2: Here I am editing master branch to make sure the version of the file in the slave branch (with its changes) is out of date compared to this.
