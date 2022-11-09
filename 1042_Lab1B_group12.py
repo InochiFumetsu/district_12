e809d129d41c7abdefd6a4ca8614e1058ef9bb5b
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 16:29:10 -0500

    Update 1042_Lab1B_group12.py
    formatting.

--- 1042_Lab1B_group12.py
+++ 1042_Lab1B_group12.py
@@ -2,7 +2,7 @@
 #version: 0.0
 
 import importlib as imp               # Spencer: I am not sure of the syntax they will teach us for this in class (don't know if this is how they'll want us to do it)
-imp.import_module(load_data_12)    #          I'm just including it to kind of show what I *THINK* they're going to have us do for the 2 "modules" we have to write.
+imp.import_module(load_data_12)       #          I'm just including it to kind of show what I *THINK* they're going to have us do for the 2 "modules" we have to write.
 imp.import_module(sort_plot_12)
 
 # Ok so it SEEMS as though you only have to remove the ">>>>>>>master" and "<<<<<<<slave" lines of "code" in the file it shows you highlighting the conflicts in
