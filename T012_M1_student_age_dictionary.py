9b53a513b0763236dca76a530aa78c7fa1b0dacc
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 21:12:01 -0500

    Update T012_M1_student_age_dictionary.py

--- T012_M1_student_age_dictionary.py
+++ T012_M1_student_age_dictionary.py
@@ -1,1 +1,16 @@
 #STUDENT AGE DICTIONARY FILE
+
+"""
+The keys of the dictionary are the students' ages.
+A student's age range is from 15 to 22.
+See below for the sample output of the function:
+{ 15 : [ {'School': 'GP', 'StudyTime': 4.2, 'Failures': 3,
+'Health': 3, 'Absences': 6, 'G1': 7, 'G2': 8, 'G3': 10},
+{another element},
+… ],
+16 : [ {'School': 'MS', 'StudyTime': 1, 'Failures': 1.2,
+'Health': 4, 'Absences': 10, 'G1': 9, 'G2': 11, 'G3': 7},
+{another element},
+… ],
+… }
+"""
