2c6666af0beeab8891aaeaacc87d6863eaeee3ce
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 20:00:58 -0500

    Update 1042_Lab1B_group12.py

--- 1042_Lab1B_group12.py
+++ 1042_Lab1B_group12.py
@@ -5,6 +5,4 @@ import importlib as imp               # Spencer: I am not sure of the syntax the
 imp.import_module(load_data_12)       #          I'm just including it to kind of show what I *THINK* they're going to have us do for the 2 "modules" we have to write.
 imp.import_module(sort_plot_12)
 
-# Ok so it SEEMS as though you only have to remove the ">>>>>>>master" and "<<<<<<<slave" lines of "code" in the file it shows you highlighting the conflicts in
-# order to have the option to mark the file as "conflicts resolved" (top right button) -- actually, on further testing, you only need to remove the "<<<<<<<"
-# and ">>>>>>>" -- apart from that you just rewrite whatever's in the file to be the way you want it and mark it as "resolved" --> then you can merge!
+# aaaand here we're changing the slave file to check GitHub response to master >>>>>>> slave PR.
