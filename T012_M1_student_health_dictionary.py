20779d5b1a42489dd4d0f0090297fe0f222c7306
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 21:13:20 -0500

    Update T012_M1_student_health_dictionary.py

--- T012_M1_student_health_dictionary.py
+++ T012_M1_student_health_dictionary.py
@@ -1,1 +1,15 @@
 #STUDENT HEALTH DICTIONARY FILE
+
+"""
+The keys of the dictionary are the students' health numbers.
+A student's health range is from 1 (meaning very bad) to 5 (meaning very good).
+See below for the sample output of the function:
+{ 1 : [ {'School':'GP', 'Age': 17, 'StudyTime': 4.2, 'Failures': 3, 'Absences': 6, 'G1': 7, 'G2': 8, 'G3': 10 },
+{another element},
+… ],
+2 : [ {'School': 'MS', 'Age': 20, 'StudyTime': 1.2, 'Failures': 1,
+'Absences': 10, 'G1': 9, 'G2': 11, 'G3': 7},
+{another element},
+… ],
+… }
+"""
