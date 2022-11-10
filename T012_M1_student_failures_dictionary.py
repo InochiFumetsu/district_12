d9e011b8ed9e0e264e8909219fc2033488c34c14
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 21:12:32 -0500

    Update T012_M1_student_failures_dictionary.py

--- T012_M1_student_failures_dictionary.py
+++ T012_M1_student_failures_dictionary.py
@@ -1,1 +1,16 @@
 #STUDENT FAILURES DICTIONARY FILE
+
+"""
+The keys of the dictionary are the number of past failures the students have had.
+The function assumes that past failures range from 0 to 10.
+See below for the sample output of the function:
+{ 0 : [ {'School': 'GP', 'Age': 18, 'StudyTime': 6.7, 'Failures': 0,
+'Health': 3, 'Absences': 7, 'G1': 12, 'G2': 13, 'G3': 14},
+{another element},
+… ],
+1 : [ {'School': 'MS', 'Age': 21, 'StudyTime': 1.8, 'Failures': 2,
+'Health': 2, 'Absences': 3, 'G1': 11, 'G2': 11, 'G3': 8},
+{another element},
+… ],
+… }
+"""
