0f332c18557703760277fc68b1df5b8e716a8027
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-09 21:13:55 -0500

    Update T012_M1_student_school_dictionary.py

--- T012_M1_student_school_dictionary.py
+++ T012_M1_student_school_dictionary.py
@@ -1,1 +1,14 @@
 #STUDENT SCHOOL DICTIONARY FILE
+
+"""
+The keys of the dictionary are the initials of the schools the students attended.
+The function assumes that only the schools listed in the data set provided (student-mat.csv) exist for the keys.
+See below for the sample output of the function:
+{ 'GP' : [ {'Age': 18, 'StudyTime': 1.8, 'Failures': 0, 'Health': 3, 'Absences': 6, 'G1': 5, 'G2': 6, 'G3': 6 },
+{another element},
+… ],
+'MB' : [ {'Age': 16, 'StudyTime': 2.6, 'Failures': 0, 'Health': 3, 'Absences': 12, 'G1': 10, 'G2': 12, 'G3': 12 },
+{another element},
+… ],
+… }
+"""
