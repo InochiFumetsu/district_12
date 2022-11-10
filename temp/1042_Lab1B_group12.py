07d621d2f1d441e83173fe837c3349e4e9ee17d8
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-10 16:38:44 -0500

    Update 1042_Lab1B_group12.py

--- temp/1042_Lab1B_group12.py
+++ temp/1042_Lab1B_group12.py
@@ -4,4 +4,4 @@
 import importlib as imp                            # Spencer: I am not sure of the syntax they will teach us for this in class 
                                                    #          (don't know if this is how they'll want us to do it) I'm just 
 load_data = imp.import_module(T012_M1_load_data)   #          including it to kind of show what I *THINK* they're going to have 
-sort_plot = imp.import_module(sort_plot_12)        #          us do for the 2 "modules" we have to write.
+sort_plot = imp.import_module(T012_M1_sort_plot)   #          us do for the 2 "modules" we have to write.
