1775c325f14f72900230a84da5ec2fc0819aaa5f
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-14 17:36:22 -0500

    Update T012_M1_student_age_dictionary.py

--- individual/spencer/T012_M1_student_age_dictionary.py
+++ individual/spencer/T012_M1_student_age_dictionary.py
@@ -4,13 +4,7 @@
 # STUDENT NO.: 101230073
 
 """
-3. Submit your function (Txxx_M1_yyy.py) on Brightspace (individual submission of your assigned function). Before you submit, ensure:
-  a. Your name and student number are written at the top of the file as a Python comment.
-  b. The function has the proper name.
   c. You have included type annotations and docstring. Do not forget to include the preconditions, if any.
-  d. At the end of the file, you have included the main script which calls the function.
-  e. The file name is correct.
-  f. Run Txxx_M1_yyy.py one more time to ensure that there are no errors!
 """
 
 
@@ -50,3 +44,7 @@ def student_age_dictionary(file_name: str) -> dict:
             del age_dictionary[entries[i]['Age']][0]['Age']
 
     return age_dictionary
+
+
+# MAIN SCRIPT (CALLING FUNCTION)
+student_age_dictionary = student_age_dictionary('student-mat.csv')
