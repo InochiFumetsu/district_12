d20735230dd50403ca1281922e8c9e9a32817d34
Esteban Heidrich <117683649+Hey1121@users.noreply.github.com>
2022-11-14 17:16:22 -0500

    Update T012_M1_student_health_dictionary.py

--- individual/esteban/T012_M1_student_health_dictionary.py
+++ individual/esteban/T012_M1_student_health_dictionary.py
@@ -1,32 +1,70 @@
 #STUDENT HEALTH DICTIONARY FILE
 
-#AUTHOR: <YOUR-NAME-HERE>
+#AUTHOR: <Esteban>
 
-"""
-The keys of the dictionary are the students' health numbers.
-A student's health range is from 1 (meaning very bad) to 5 (meaning very good).
-See below for the sample output of the function:
-{ 1 : [ {'School':'GP', 'Age': 17, 'StudyTime': 4.2, 'Failures': 3, 'Absences': 6, 'G1': 7, 'G2': 8, 'G3': 10 },
-{another element},
-… ],
-2 : [ {'School': 'MS', 'Age': 20, 'StudyTime': 1.2, 'Failures': 1,
-'Absences': 10, 'G1': 9, 'G2': 11, 'G3': 7},
-{another element},
-… ],
-… }
+import string
+from typing import List
 
-1. Follow the FDR to implement the function you have been assigned. (Note that FDR was covered in ECOR1041).
-  a. You can use “import string” and “from typing import List” only. If you think other libraries are needed, you must ask the TAs for approval. Note: You can treat a 
-     CSV file as a text file; just open it with Notepad instead of Excel to check how the data is organized.
-  b. Assume that your data does not contain duplicated entries.
-  c. Required Filename: Txxx_M1_yyy.py where xxx is your team identifier (e.g., 035) and yyy = {student_age_dictionary, student_health_dictionary, 
-     student_school_dictionary, or student_failures_dictionary} based on the function that you are implementing.
-2. Test your function. For this lab, you can just call your function to ensure that the data is loaded correctly. For testing purposes, use student-mat.csv.
-3. Submit your function (Txxx_M1_yyy.py) on Brightspace (individual submission of your assigned function). Before you submit, ensure:
-  a. Your name and student number are written at the top of the file as a Python comment.
-  b. The function has the proper name.
-  c. You have included type annotations and docstring. Do not forget to include the preconditions, if any.
-  d. At the end of the file, you have included the main script which calls the function.
-  e. The file name is correct.
-  f. Run Txxx_M1_yyy.py one more time to ensure that there are no errors!
-"""
+def student_health_dictionary(file_name: str) -> dict:
+    """This function takes student information and sorts them by their health 
+    condition. 
+    
+    precondtions: file_name must be an existing file in the same folder as 
+    python
+    
+    >>>student_health_dictionary(student_info)
+    {'3': [{'School': 'GP', 'Age': '18', 'StudyTime': '2', 'Failures': '0',
+    'Absences': '6', 'G1': '5', 'G2': '6', 'G3': '6'}, {another element},
+    ...], 
+    {'5': [{'School': 'GP', 'Age': '15', 'StudyTime': '3', 'Failures': '0',
+    'Absences': '2', 'G1': '15', 'G2': '14', 'G3': '15'}, {another element},
+    ...], 
+    ...}
+    """
+    health_dictionary = {}
+    first_run = True
+    counter_1 = 0
+    counter_2 = 0
+    counter_3 = 0
+    raw_data = []
+    file_content = open(file_name, "r")
+    for line in file_content:
+        counter_1 += 1
+    raw_data = [0] * counter_1
+    file_content = open(file_name, "r")
+    for line in file_content:
+        if first_run:
+            line = line.strip()
+            student_dict_keys = line.split(",")
+            first_run = False
+        raw_data[counter_2] = line.strip()
+        counter_2 += 1
+    for j in raw_data:
+        student_stat_1 = j.split(",")
+        health = student_stat_1[4]
+        if not health in health_dictionary:
+            health_dictionary[health] = []
+            health_dictionary[health].append({
+                student_dict_keys[0]: student_stat_1[0],
+                student_dict_keys[1]: student_stat_1[1],
+                student_dict_keys[2]: student_stat_1[2],
+                student_dict_keys[3]: student_stat_1[3],
+                student_dict_keys[5]: student_stat_1[5],
+                student_dict_keys[6]: student_stat_1[6],
+                student_dict_keys[7]: student_stat_1[7],
+                student_dict_keys[8]: student_stat_1[8]
+            })
+        else:
+            health_dictionary[health].append({
+                student_dict_keys[0]: student_stat_1[0],
+                student_dict_keys[1]: student_stat_1[1],
+                student_dict_keys[2]: student_stat_1[2],
+                student_dict_keys[3]: student_stat_1[3],
+                student_dict_keys[5]: student_stat_1[5],
+                student_dict_keys[6]: student_stat_1[6],
+                student_dict_keys[7]: student_stat_1[7],
+                student_dict_keys[8]: student_stat_1[8]
+            })
+        counter_3 += 1
+    del health_dictionary["Health"]
+    return health_dictionary
