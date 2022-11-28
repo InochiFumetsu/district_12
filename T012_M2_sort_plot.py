76e3a65314f2400523629fe3d4af4d5bcdbc72b4
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-11-28 17:26:57 -0500

    Update T012_M2_sort_plot.py

--- T012_M2_sort_plot.py
+++ T012_M2_sort_plot.py
@@ -1,15 +1,63 @@
+#LOAD DATA MODULE
+#AUTHORS:
+#   Spencer Hiscox    101230073
+#   Jack Roberts      101261505
+#   Esteban Heidrich  101267959
+#   Milan Djordjevic  101262178
+
+
+
 import T012_M1_load_data
+from numpy import polyfit
+
 
 def student_list(i_dict: dict) -> list:
+    """Return list compilation of all student data entries contained in the 
+    dictionary passed to the function in the first position argument ('i_dict')
+    which contain the appropriate entries for the key from which the dictionary
+    passed in was created.
+    
+    Preconditions: 1) Dictionary passed into function as 'i_dict' must be a 
+        dictionary generated using one of the four functions defined in 
+        T012_M1_load_data.py
+    
+    >>>student_list(T012_M1_load_data.student_age_dictionary('student-mat.csv'))
+    [{'School': 'GP', 'StudyTime': 2.0, 'Failures': 0, 'Health': 3, 'Absences': 
+    6, 'G1': 5.0, 'G2': 6.0, 'G3': 6.0, 'Age': 18}, {'School': 'MB', 
+    'StudyTime': 1.0, 'Failures': 2, 'Health': 4, 'Absences': 0, 'G1': 7.0, 
+    'G2': 4.0, 'G3': 0.0, 'Age': 18}, ... ,
+    {'School': 'MS', 'StudyTime': 1.0, 'Failures': 3, 'Health': 3, 'Absences': 
+    3, 'G1': 10.0, 'G2': 8.0, 'G3': 7.0, 'Age': 21}]
+    >>>student_list(T012_M1_load_data.student_school_dictionary(
+    'student-mat.csv'))
+    [{'Age': 18, 'StudyTime': 2.0, 'Failures': 0, 'Health': 3, 'Absences': 6, 
+    'G1': 5.0, 'G2': 6.0, 'G3': 6.0, 'School': 'GP'}, {'Age': 17, 'StudyTime': 
+    2.0, 'Failures': 0, 'Health': 3, 'Absences': 4, 'G1': 5.0, 'G2': 5.0, 'G3': 
+    6.0, 'School': 'GP}, ... ,
+    {'Age': 19, 'StudyTime': 1.0, 'Failures': 0, 'Health': 5, 'Absences': 5, 
+    'G1': 8.0, 'G2': 9.0, 'G3': 9.0, 'School': 'MS'}]
+
     """
-    """
+    if type(i_dict) != dict:
+        raise TypeError("Invalid argument: parameter <i_dict> (argument in "
+                         "position 1) must be of type <dict>")        
+    
     expected = {}
     metric = ''
     actual = {}
     key_fail = False
     with open('student-mat.csv', 'r') as file:
         keys = file.readline().strip("\n ").split(sep=",")
+        
         for dict_key in iter(i_dict):
+            if type(i_dict[dict_key]) != list:
+                raise ValueError("Invalid argument: parameter <i_dict> (argument in"
+                        " position 1) must be a dictionary whose values are lists.")
+            if type(i_dict[dict_key][0]) != dict:
+                raise ValueError("Invalid argument: parameter <i_dict> "
+                                 "(argument in position 1) must be a dictionary"
+                                 " whose values are lists of dictionaries.")
+            
             adjusted = []
             for key in i_dict[dict_key][0]:
                 adjusted.append(str(key.strip(" \n").replace(" ", "")))
@@ -51,17 +99,19 @@ def student_list(i_dict: dict) -> list:
                     expected[key] = 1
                     
     if key_fail:
-        print("\n\n\nInvalid dictionary: Primary key mismatch. Empty list returned.\n")
-        input("Press <ENTER> to continue.\n\n")
-        return []
+        raise ValueError("Invalid argument: parameter <i_dict> (argument in "
+                         "position 1) must be a dictionary whose keys match the"
+                         " column headers in 'student-mat.csv' file.")
     
     output_list = []
-    for key in i_dict.keys():
-        for entry in i_dict[key]:
+    for key in i_dict:            
+        for entry in i_dict[key]:             
             entry[metric] = key
             output_list.append(entry)
             
     return output_list
+
+
     
 def sort_students_bubble(my_dict: dict, attribute: str) -> list:
     """
@@ -102,6 +152,8 @@ def sort_students_bubble(my_dict: dict, attribute: str) -> list:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 swap = True  # swapping is not done yet
     return arr
+    
+    
     
 def sort_students_selection(dic: dict, key: str) -> dict:
     """
@@ -144,9 +196,116 @@ def sort_students_selection(dic: dict, key: str) -> dict:
 
     return dictionary  # Return sorted dictionary
 
-if __name__ == "__main__":
-    age_result = student_list(T012_M1_load_data.student_age_dictionary('student-mat.csv'))
-    school_result = student_list(T012_M1_load_data.student_school_dictionary('student-mat.csv'))
-    failures_result = student_list(T012_M1_load_data.student_failures_dictionary('student-mat.csv'))
-    health_result = student_list(T012_M1_load_data.student_health_dictionary('student-mat.csv'))
+
+
+def curve_fit(i_dict: dict, metric: str, degree: int) -> list:
+    """Return a list of the coefficients of the variables for the degree 
+    polynomial specified as 'degree' argument, defined by performing polynomial
+    regression of average 'G_Avg' for each student associated with a particular
+    metric specified as 'metric' argument. If specified 'degree' is higher than
+    number of data points available from which to perform a regression, the 
+    regression will be performed with the degree of the resultant polynomial 
+    equal to the number of data points available in the data set.
     
+    Preconditions: 1) Dictionary passed into function as 'i_dict' must be a 
+        dictionary generated using one of the four functions defined in 
+        T012_M1_load_data.py
+        2) The 'metric' selected for the second position argument must be one 
+        of the keys contained within the returned list from student_list(i_dict)
+    
+    >>>curve_fit(T012_M1_load_data.student_age_dictionary('student-mat.csv'), 
+    'Health', 3)
+    [-0.0278538498754179, 0.46987985738198207, -2.2742147821236243, 
+    13.7162164641214]
+    >>>curve_fit(T012_M1_load_data.student_age_dictionary('student-mat.csv'), 
+    'Failures', 2)
+    [0.35420696644042277, -2.729867788461538, 11.356343090120658]
+    >>>curve_fit(T012_M1_load_data.student_age_dictionary('student-mat.csv'), 
+    'Failures', 4)
+    x-wingide-python-shell://129836008/6:1: RankWarning: Polyfit may be poorly 
+    conditioned
+    [-0.0129539441809042, 0.0769655894192614, 0.21512492094821192, 
+    -2.6557070790070747, 11.356570512820507]
+    >>>curve_fit(T012_M1_load_data.student_age_dictionary('student-mat.csv'), 
+    'Failures', 32)
+    x-wingide-python-shell://129836008/6:1: RankWarning: Polyfit may be poorly 
+    conditioned
+    [-0.0129539441809042, 0.0769655894192614, 0.21512492094821192, 
+    -2.6557070790070747, 11.356570512820507]
+    """
+    if type(i_dict) != dict:
+        raise TypeError("Invalid argument: parameter <i_dict> (argument in "
+                         "position 1) must be of type <dict>")    
+    
+    for key in i_dict:
+        if type(i_dict[key]) != list:
+            raise ValueError("Invalid argument: parameter <i_dict> (argument in"
+                    " position 1) must be a dictionary whose values are lists.")
+        for i in range(len(i_dict[key])):
+            if type(i_dict[key][i]) != dict:
+                raise ValueError("Invalid argument: parameter <i_dict> "
+                                 "(argument in position 1) must be a dictionary"
+                                 " whose values are lists of dictionaries.")                
+            if not 'G_Avg' in i_dict[key][i]:
+                i_dict = T012_M1_load_data.add_average(i_dict)
+            break
+        break
+    
+    i_dict = student_list(i_dict)
+    
+    if not metric in i_dict[0]:
+        raise ValueError("Invalid argument: parameter <metric> (argument in "
+                         "position 2) not contained in i_dict.")
+    if type(i_dict[0][metric]) != int and type(i_dict[0][metric]) != float:
+        raise ValueError("Invalid argument: parameter <metric> (argument in "
+                         "position 2) must be a key whose values are numeric")
+    if type(metric) != str:
+        raise TypeError("Invalid argument: parameter <metric> (argument in "
+                         "position 2) must be of type <string>")
+    if type(degree) != int:
+        raise TypeError("Invalid argument: parameter <degree> (argument in "
+                         "position 3) must be of type <int>")
+    
+    
+    dataset = {}
+    for entry in i_dict:
+        if entry[metric] in dataset:
+            dataset[entry[metric]][0] += entry['G_Avg']
+            dataset[entry[metric]][1] += 1
+            continue
+        dataset[entry[metric]] = [entry['G_Avg'], 1 ]
+            
+    for key in dataset:
+        dataset[key] = dataset[key][0] / dataset[key][1]
+        
+    num_data_points = len(dataset)
+    if num_data_points < degree:
+        degree = num_data_points
+        
+    return list(polyfit(list(dataset.keys()), list(dataset.values()), degree))
+
+
+
+#FUNCTION CALLS HERE
+if __name__ == "__main__":
+    dictionaries = [T012_M1_load_data.student_age_dictionary('student-mat.csv'), 
+                    T012_M1_load_data.student_school_dictionary('student-mat.csv'), 
+                    T012_M1_load_data.student_failures_dictionary('student-mat.csv'), 
+                    T012_M1_load_data.student_health_dictionary('student-mat.csv')]
+    
+    metrics = ['StudyTime', 'Health', 'Absences', 'Failures', 'Age', 'School', 'G1', 'G2', 'G3']
+    not_numeric = ['School', 'G1', 'G2', 'G3']
+    
+    curve_fit_results = []
+    selection_sort_results = []
+    bubble_sort_results = []
+    student_list_results = []
+    for dictionary in dictionaries:
+        student_list_results.append(student_list(dictionary))
+        for metric in metrics:
+            selection_sort_results.append(sort_students_selection(dictionary, metric))
+            bubble_sort_results.append(sort_students_bubble(dictionary, metric))
+            for degree in range(1, 6):
+                if not metric in not_numeric:
+                    curve_fit_results.append(curve_fit(dictionary, metric, degree))
+                
