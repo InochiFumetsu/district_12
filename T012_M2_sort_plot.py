c0a67da9170de83c3295e468a3e9e900bd2fbfec
MilanxDjordjevic <118127027+MilanxDjordjevic@users.noreply.github.com>
2022-11-28 16:10:27 -0500

    Update T012_M2_sort_plot.py

--- T012_M2_sort_plot.py
+++ T012_M2_sort_plot.py
@@ -62,6 +62,46 @@ def student_list(i_dict: dict) -> list:
             output_list.append(entry)
             
     return output_list
+    
+def sort_students_bubble(my_dict: dict, attribute: str) -> list:
+    """
+    Return a sorted dictoinary in ascending order or alphabetical order by using bubble sort, based off the passed attribute.
+    Precondition: attribute is a valid/ accepted key in my_dict and the passed dictionary follows specific format (dictionary with lists as keys, each containing dictionaries)
+
+    >>>sort_students_bubble(T012_M1_load_data.student_failures_dictionary('student-mat.csv'), "Age")
+    [{'School': 'GP', 'Age': 15, 'StudyTime': 3.0, 'Health': 5, 'Absences': 2, 'G1': 15.0, 'G2': 14.0, 'G3': 15.0, 'Failures': 0}, 
+    {'School': 'GP', 'Age': 15, 'StudyTime': 2.0, 'Health': 1, 'Absences': 0, 'G1': 16.0, 'G2': 18.0, 'G3': 19.0, 'Failures': 0},
+    ...
+    ...
+    ]
+    >>>sort_students_bubble(T012_M1_load_data.student_age_dictionary('student-mat.csv'), "Health")
+    [{'School': 'BD', 'StudyTime': 2.0, 'Failures': 0, 'Health': 1, 'Absences': 13, 'G1': 17.0, 'G2': 17.0, 'G3': 17.0, 'Age': 18}, 
+    {'School': 'BD', 'StudyTime': 1.0, 'Failures': 0, 'Health': 1, 'Absences': 8, 'G1': 10.0, 'G2': 11.0, 'G3': 10.0, 'Age': 18}, 
+    ...
+    ...
+    ]
+    >>>sort_students_bubble(T012_M1_load_data.add_average(T012_M1_load_data.student_age_dictionary('student-mat.csv')), "Health")
+    [{'School': 'BD', 'StudyTime': 2.0, 'Failures': 0, 'Health': 1, 'Absences': 13, 'G1': 17.0, 'G2': 17.0, 'G3': 17.0, 'G_Avg': 17.0, 'Age': 18}, 
+    {'School': 'BD', 'StudyTime': 1.0, 'Failures': 0, 'Health': 1, 'Absences': 8, 'G1': 10.0, 'G2': 11.0, 'G3': 10.0, 'G_Avg': 10.33, 'Age': 18}, 
+    {'School': 'BD', 'StudyTime': 1.0, 'Failures': 0, 'Health': 1, 'Absences': 5, 'G1': 16.0, 'G2': 15.0, 'G3': 16.0, 'G_Avg': 15.67, 'Age': 18}, 
+    ...
+    ...
+    ]
+
+    """
+
+    # converts dictionary of list of dictionaries to a list of dictionaries
+    arr = T012_M2_sort_plot.student_list(my_dict)
+
+    swap = True
+    while swap:
+        swap = False  # assumes algorithm is done
+        for i in range(len(arr) - 1):
+            if arr[i][attribute] > arr[i + 1][attribute]:  # compares side by side items in list
+                # swaps them if necessary
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swap = True  # swapping is not done yet
+    return arr
 
 if __name__ == "__main__":
     age_result = student_list(T012_M1_load_data.student_age_dictionary('student-mat.csv'))
