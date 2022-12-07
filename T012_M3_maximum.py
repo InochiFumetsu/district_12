3ddde3ba879395628efd487d2cc42d4055037905
InochiFumetsu <117765873+InochiFumetsu@users.noreply.github.com>
2022-12-06 21:05:13 -0500

    Update T012_M3_maximum.py

--- T012_M3_maximum.py
+++ T012_M3_maximum.py
@@ -2,6 +2,7 @@
 #    Spencer Hiscox    101230073
 
 import T012_M1_load_data as ld, T012_M2_sort_plot as sp, scipy.optimize as scop
+from typing import Dict, List
 
 
 
@@ -25,14 +26,16 @@ def evaluate_negate_quadratic(x: float, a: float, b: float, c: float)->float:
 
 
 
-def maximum(attribute: str)->tuple:
+def maximum(attribute: str, i_dict: Dict[int or str, List[dict]])->tuple:
     """Return (a, b) where a = the value of 'attribute' within the data 
     contained in student-mat.csv file which pertains to the highest average
     grade obtained within that attribute, b = the highest average grade 
     (the grade obtained by a).
     
-    Preconditions: attribute must exactly equal one of the column headers in 
+    Preconditions: 1) attribute must exactly equal one of the column headers in 
         the student-mat.csv file
+        2) i_dict must be a dictionary created using one of the functions 
+        defined in T012_M1_load_data.py
     
     >>> maximum('Health')
     (1.0, 11.85)
@@ -50,17 +53,29 @@ def maximum(attribute: str)->tuple:
     (0.0, 11.5)
 
     """
-    dictionary = ld.add_average(ld.student_age_dictionary("student-mat.csv"))
-    dictionary2 = ld.add_average(ld.student_age_dictionary("student-mat.csv"))
+    for key in i_dict:
+        for entry in i_dict[key]:
+            if not 'G_Avg' in entry:
+                i_dict = ld.add_average(i_dict)
+            break
+        break
     
-    raw_data = sp.student_list(dictionary)
+    i_dict_copy = {}
+    for key in i_dict:
+        for entry in i_dict[key]:
+            if key in i_dict_copy:
+                i_dict_copy[key].append(entry.copy())
+                continue
+            i_dict_copy[key] = [entry.copy()] 
+    
+    raw_data = sp.student_list(i_dict)
     values = set()
     for entry in raw_data:
         values.add(entry[attribute])
         
     minimum, maximum = min(values), max(values)
     
-    coefficients = tuple(sp.curve_fit(dictionary2, attribute, 2))
+    coefficients = tuple(sp.curve_fit(i_dict_copy, attribute, 2))
     
     regression_max = scop.fminbound(
         evaluate_negate_quadratic, minimum, maximum, args=coefficients)
@@ -72,7 +87,25 @@ def maximum(attribute: str)->tuple:
 #MAIN SCRIPT
 if __name__ == "__main__":
     attributes = ['Health', 'Age', 'StudyTime', 'Failures', 'Absences']
-    results = []
+    results = {}
+    first_run = True
     for attribute in attributes:
-        results.append(maximum(attribute))
-        
+        if first_run:
+            first_run = False
+            results['Age'] = [maximum(attribute, 
+                           ld.student_age_dictionary("student-mat.csv"))]
+            results['School'] = [maximum(attribute, 
+                           ld.student_school_dictionary("student-mat.csv"))]
+            results['Failures'] = [maximum(attribute, 
+                           ld.student_failures_dictionary("student-mat.csv"))]
+            results['Health'] = [maximum(attribute, 
+                           ld.student_health_dictionary("student-mat.csv"))]
+            continue
+        results['Age'].append(maximum(attribute, 
+                       ld.student_age_dictionary("student-mat.csv")))
+        results['School'].append(maximum(attribute, 
+                       ld.student_school_dictionary("student-mat.csv")))
+        results['Failures'].append(maximum(attribute, 
+                       ld.student_failures_dictionary("student-mat.csv")))
+        results['Health'].append(maximum(attribute, 
+                       ld.student_health_dictionary("student-mat.csv")))
